8148324bf72869225857377987f423c89a79f6e2
Shrut <shrutpatel20@gmail.com>
2024-07-22 17:42:51 -0500

    make chart helper

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,10 +4,11 @@ import { mockCompanyDetails } from "../constants/mock";
 import Search from "./Search";
 import Header from "./Header";
 import Details from "./Details";
+import Overview from "./Overview";
 
 const Dashboard = () => {
   return (
-    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
+    <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand bg-neutral-100">
       <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
         <Header name={mockCompanyDetails.name}/>
       </div>
@@ -15,7 +16,7 @@ const Dashboard = () => {
         <Card>Chart</Card>
       </div>
       <div>
-        <Card>Overview</Card>
+        <Overview symbol={mockCompanyDetails.ticker} price={300} change={20} changePercent={10.0} currency={"USD"}/>
       </div>
       <div className="row-span-2 xl:row-span-3">
         <Details details={mockCompanyDetails}/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Search from './Search'
+import ThemeIcon from './ThemeIcon'
 
 const Header = ({name}) => {
   return <>
@@ -7,6 +8,7 @@ const Header = ({name}) => {
         <h1 className="text-5xl">{name}</h1>
         <Search />
     </div>
+    <ThemeIcon/>
   </>
 }
 
